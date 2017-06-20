6a35442183a203b05fdcccb5f520ba3e9bf97650
Sam Skjonsberg <skoneberg@gmail.com>
2017-06-20 10:01:20 -0700

    Fix gulpfile, so that it doesn't require HTTP auth. (#30)
    * Fix gulpfile, so that it doesn't require HTTP auth.
    
    * Keep dem options.

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ gulp.task('cname', () => {
 });
 
 gulp.task('deploy', ['cname', 'html', 'sass', 'assets'], () => {
-  const DEPLOY_OPTS = { remoteUrl: "https://github.com/allenai/oasp-website.git" };
+  const DEPLOY_OPTS = { remoteUrl: "git@github.com/allenai/oasp-website" };
 
   return gulp.src(`${BUILD_DIR}/**/*`)
     .pipe(deployToGithubPages(DEPLOY_OPTS));
